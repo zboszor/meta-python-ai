b779e79870cb8b726092f1180931d50ece5fecb4
Zoltán Böszörményi <zboszor@gmail.com>
2024-04-09 17:46:25 +0200

    python3-yolov5: New recipe for version 7.0.12
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ recipes-python/yolov5/python3-yolov5/setup.py
@@ -0,0 +1,61 @@
+import io
+import os
+import re
+
+import setuptools
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+def get_requirements():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    reqs = os.path.join(base_dir, "requirements.txt")
+    with open(reqs) as f:
+        return f.read().splitlines()
+
+
+def get_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(current_dir, "yolov5", "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
+setuptools.setup(
+    name="yolov5",
+    version=get_version(),
+    author="",
+    license="GPL",
+    description="Packaged version of the Yolov5 object detector",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ultralytics/yolov5",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    python_requires=">=3.7",
+    install_requires=get_requirements(),
+    include_package_data=True,
+    options={'bdist_wheel':{'python_tag':'py37.py38.py39.py310.py311'}},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+    ],
+    keywords="machine-learning, deep-learning, ml, pytorch, YOLO, object-detection, vision, YOLOv5, YOLOv7",
+)
